a7648ad978dee09f98af7940826693acc54d5635
Miredly <miredly@Yuja.local>
2020-07-10 13:20:45 -1000

    Added jumping; test grid texture

--- Assets/Scripts/FP_Controller.cs
+++ Assets/Scripts/FP_Controller.cs
@@ -9,6 +9,7 @@ public class FP_Controller : MonoBehaviour
 
     public float move_speed = 0.01f;
     public float mouse_sensitivity = 1.0f;
+    public float jump_power = 1.0f;
 
 
     void Start()
@@ -21,15 +22,20 @@ public class FP_Controller : MonoBehaviour
     {
         //mouse x and y are not game-space x and y
         float ry = Input.GetAxis("Mouse X");
-        float rx = Input.GetAxis("Mouse Y");
-        
+        float rx = Input.GetAxis("Mouse Y");  
         float move_lr = Input.GetAxis("Horizontal");
         float move_fb = Input.GetAxis("Vertical");
+        bool jump = Input.GetButtonDown("Jump");
 
-        gameObject.transform.Rotate(0, ry, 0, Space.Self);
-        cam.transform.Rotate(-rx, 0, 0, Space.Self);
+        bool grounded = true;
+
+        gameObject.transform.Rotate(0, ry * mouse_sensitivity, 0, Space.Self);
+        cam.transform.Rotate(-rx * mouse_sensitivity * 0.75f, 0, 0, Space.Self);
 
         transform.Translate(new Vector3(move_lr * move_speed, 0, move_fb * move_speed));
 
+        if(jump && grounded){
+            rb.velocity = new Vector3(0, jump_power, 0);
+        }
     }
 }
