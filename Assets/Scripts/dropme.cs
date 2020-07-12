df6ea4d33f8ee8a9e7715c91531e11c4a189ed40
Kurt Noe <31440372+KurtsCode@users.noreply.github.com>
2020-07-11 17:02:10 -1000

    Created some test files to test interactions and implemented chelves that can hold objects and can be opened or closed.

---
+++ Assets/Scripts/dropme.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropme : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other){
+        if(other.collider.CompareTag("Player") || other.collider.CompareTag("Environment")){
+            //if we have a parent, we're probably being held?
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            gameObject.transform.parent.transform.DetachChildren();
+        }
+    }
+}
